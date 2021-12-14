d5d8d43e41ffcb8213f18c6c5cf2cf5387230f0e
Edward Lynch-Milner <eddylynch9@gmail.com>
2021-12-14 11:16:10 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ethics-application-system-backend
+This repository is the location of the back-end (server) development work done for my CSIS Final Year Project implementing an Ethics Application Submission and Managamenet System for the Faculty of Science and Engineering's Ethics Committee
---
+++ src/main/java/ie/ul/edward/Application.java
@@ -0,0 +1,14 @@
+package ie.ul.edward;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * This is the main entrypoint class into our backend
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
